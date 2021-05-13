a4b0e7de763e4409d9b972f6e05e3d23803ebe24
Laura <lvna.95@gmail.com>
2021-05-13 18:26:46 -0500

    segudo commit

--- src/App.js
+++ src/App.js
@@ -2,11 +2,17 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
+  state = {
+    valor: 0
+  }
+  handleIncrement = () =>{
+    this.setState({valor: this.state.valor + 1})
+  }
   render() {
     return (
       <div>
-        <span className="value">0</span>
-        <button id="inc">Incrementa</button>
+        <span className="value">{this.state.valor}</span>
+        <button id="inc" onClick={this.handleIncrement}>Incrementa</button>
       </div>
     );
   }
